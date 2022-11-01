ca37ad68863e9dc724a6ce492eda29e98532afdd
Nyerhowho9 <nyerhowho9@gmail.com>
2022-11-01 04:07:35 +0100

    Rendered the component sin app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import icon from '../src/images/Icon.png';
+import Profile from './components/profile';
+import Links from './components/links';
+import Footer from './components/footer';
+import '../src/styles/main.css';
 
 function App() {
   return (
     <div className="App">
-     
+        <img id='forward-icon'  alt="" src={icon}/>
+        <Profile twittername="ny_wura"  />
+        <Links />
+        <Footer/>
     </div>
   );
 }
